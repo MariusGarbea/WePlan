6e686722f263a64cad436d5e2670328be4628d52
Marius <mariusgarbea@ymail.com>
2017-11-05 06:50:40 -0500

    improved vote

--- frontend/src/Components/Vote.js
+++ frontend/src/Components/Vote.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import '../Styles/Vote.css';
 
 import Header from './Header';
 
+var previous;
+
 class Vote extends Component {
   state = {
     result: ['shit', 'sucker', 'cock', 'dick'],
-    selection: ''
+    selection: '',
+    submit: false
   }
   componentDidMount() {
     fetch(`http://localhost:6942/event-details/${this.props.match.params[0]}`)
@@ -14,26 +18,36 @@ class Vote extends Component {
     .then(result => this.setState({result}));
   }
   handleSubmit = () => {
-    console.log(this.state.selection)
+    this.setState({submit: true})
   }
-  selectTime = (event) => {
-    this.setState({selection: event.target.innerHTML})
+  selectTime = event => {
+    console.log(event.target.id)
+    if(previous) {
+      document.getElementById(previous).style.backgroundColor = "#DC2FFF";
+    }
+    this.setState({selection: event.target.innerHTML});
+    document.getElementById(event.target.id).style.backgroundColor = "#DC2685";
+    previous = event.target.id;
   }
   render() {
     const options = this.state.result.map((data, i) => {
       return (
-        <div className="box" onClick={this.selectTime} key={i}>
+        <div className="box" onClick={this.selectTime} key={i} id={i}>
           {data}
         </div>
       )
     })
     return (
       <div>
+        <Modal isOpen={this.state.submit}>
+          <h1>Thank you for your vote! (◕‿◕)</h1>
+          <h2>You voted for {this.state.selection}</h2>
+        </Modal>
         <Header />
         <div className="center">
           {this.state.result.length !== 0 ? options : <p>Loading...</p>}
           <br />
-          <button onClick={this.handleSubmit} className="btn">Submit vote</button>
+          <button onClick={this.handleSubmit} className="btn">Vote!</button>
         </div>
       </div>
     );
